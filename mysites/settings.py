4a4889c51f1a1f5442c0d0240c45f5cfc99191ad
zihow <zzh19970411@outlook.com>
2020-04-01 15:49:03 +0800

    model manager 模型管理__

--- mysites/settings.py
+++ mysites/settings.py
@@ -29,14 +29,17 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# 添加APPS
 
 INSTALLED_APPS = [
+    #超级管理员
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
